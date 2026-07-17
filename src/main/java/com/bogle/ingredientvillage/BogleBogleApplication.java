f69da5af047592e16e8036515964e666c1d5d9de
Chungyejin <yejinchung0913@gmail.com>
2026-07-16 23:04:52 -0300

    첫 commit api 백엔드 기초 및 api 연동 확인 완료

---
+++ src/main/java/com/bogle/ingredientvillage/BogleBogleApplication.java
@@ -0,0 +1,13 @@
+package com.bogle.ingredientvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogleBogleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogleBogleApplication.class, args);
+    }
+
+}
